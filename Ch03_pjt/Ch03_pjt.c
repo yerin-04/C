eb27f4bf35c9388b87e64dfee163eb12b753036b
유예린 <irene4311@naver.com>
2023-04-02 00:16:07 +0900

    과제 3-7

--- Ch03_pjt/Ch03_pjt.c
+++ Ch03_pjt/Ch03_pjt.c
@@ -1,22 +1,20 @@
 //202378076 유예린
 //2023.04.01
-//[필수]예제모음06
+
+//(선택)3-7
 
 #include<stdio.h>
 
 int pjt03()
 {
-	printf("int 형의 크기\t\t\t ==> %d\n", sizeof(int));
-	printf("unsigned int 형의 크기\t\t ==> %d\n", sizeof(unsigned int));
-	printf("short 형의 크기\t\t\t ==> %d\n", sizeof(short));
-	printf("unsigned short 형의 크기\t ==> %d\n", sizeof(unsigned short));
-	printf("long int 형의 크기\t\t ==> %d\n", sizeof(long int));
-	printf("unsigned long int 형의 크기\t ==> %d\n", sizeof(unsigned long int));
-	printf("float 형의 크기\t\t\t ==> %d\n", sizeof(float));
-	printf("double 형의 크기\t\t ==> %d\n", sizeof(double));
-	printf("long double 형의 크기\t\t ==> %d\n", sizeof(long double));
-	printf("char 형의 크기\t\t\t ==> %d\n", sizeof(char));
-	printf("unsigned char 형의 크기\t\t ==> %d\n", sizeof(unsigned char));
+	int a;
+	float b;
+
+	a = 123.45;
+	b = 200;
+
+	printf("a의 값 ==> %d \n", a);
+	printf("b의 값 ==> %f \n", b);
 
 	return 0;
 }
