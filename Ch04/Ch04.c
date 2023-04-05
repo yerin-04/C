ca836fd108bf612f42adec0ea4406d49a6568579
유예린 <irene4311@naver.com>
2023-04-05 12:09:36 +0900

    04.05

--- Ch04/Ch04.c
+++ Ch04/Ch04.c
@@ -1,22 +1,66 @@
 //202378076 유예린
-//2023.04.03
+//2023.04.05
 //Ch04 C연산자의 이해
 
 #include <stdio.h>
 
+//기본 3-10
+/*
 int ch04()
 {
 	int a = 100, b = 200;
 	float result;
 
 	result = a / b;
+	printf(" % f\n", result);
 
+	result = a / (float)b;
 	printf("%f\n", result);
-	//정수형끼리의 계산 결과는 정수형
+
 	return 0;
 }
 
 void main()
 {
 	ch04();
+}
+*/
+
+//기본 3-11
+/*int g311()
+{
+	float a = 0.1234567890123456789012345f;
+	double b = 0.1234567890123456789012345;
+
+	printf("%30.25f\n", a);
+	printf("%30.25lf\n", b);
+
+
+	return 0;
+}
+
+void main()
+{
+	g311();
+
+}
+*/
+
+//응용 4-4
+int app404()
+{
+	int a = 10;
+	int b = 
+
+	printf("%d\n", a++);
+	//b = a를 수행한 후 a를 1 증가시킨다.
+
+	printf("%d\n", ++a);
+	//a를 1 증가시킨 후에 출력한다.
+	return 0;
+}
+
+void main()
+{
+	app404();
 }
